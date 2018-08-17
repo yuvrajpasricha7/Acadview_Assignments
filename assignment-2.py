d28ebba680422614236578546eafbc01dff142d2
yuvrajpasricha7 <yuvrajpasricha@gmail.com>
2018-08-17 15:25:57 +0530

    python assignment 2

---
+++ assignment-2.py
@@ -0,0 +1,28 @@
+#Q1
+print("My name is Yuvraj Singh")
+
+#Q2
+str1='Yuvraj' #string 1
+str2='Singh' #string 2
+print(str1+' '+str2)
+
+#Q3
+x=int(input()) #variable 1
+y=int(input()) #variable 2
+z=int(input()) #variable 3
+print(x,y,z,sep=',')
+
+#Q4
+print('''"Let's get started"''')
+
+#Q5
+s='Acadview'
+course='Python'
+fees=5000
+print(s,course,fees,sep='\n')
+
+#Q6
+pi=3.14
+r=int(input()) #radius of circle
+a=pi*r*r #area of circle
+print('Area=',a)
